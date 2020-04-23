a6d1e8bb8896d0bb943c5ec868c4742e63769b14
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2020-04-23 13:48:10 +0530

    generating views

--- whiteboard/src/Content.scala
+++ whiteboard/src/Content.scala
@@ -2,84 +2,146 @@ package whiteboard
 
 import fastparse._, NoWhitespace._
 
-sealed trait Content
+import scalatags.JsDom.all._
 
-sealed trait Span extends Content
+import org.scalajs.dom.html._
 
-sealed trait Div extends Content
+import scalajs.js.Dynamic.{global => g}
+import scalatags.JsDom.TypedTag
+
+sealed trait Content {
+  def view: Element
+}
+
+sealed trait Phrase extends Content {
+  def view: Element
+} // span
+
+sealed trait Sentence extends Content // div
 
 object Content {
+    def polyDiv(ss: Vector[Element]): TypedTag[Div] = ss match {
+    case head +: Vector()     => div(contenteditable := true)(head)
+    case init :+ last => polyDiv(init)(last)
+    case Vector()     => div(contenteditable := true)
+  }
 
-  case class Text(body: String) extends Span
+    case class Body(divs: Vector[Sentence]) extends Content{
+        def view: org.scalajs.dom.html.Element = 
+            p(polyDiv(divs.map(_.view))).render
+    }
 
-  case class Strong(body: String) extends Span
+  case class Text(body: String) extends Phrase {
+    def view: org.scalajs.dom.html.Span = span(body).render
+  }
 
-  case class Emph(body: String) extends Span
+  case class Strong(body: String) extends Phrase {
+    def view = strong(body).render
+  }
 
+  case class Emph(body: String) extends Phrase {
+    def view: org.scalajs.dom.html.Element = em(body).render
+  }
 
-  case class Heading(spans: List[Span], level: Int) extends Div
+  def polySpan(ss: Vector[Element]): TypedTag[Span] = ss match {
+    case head +: Vector() => span(head)
+    case init :+ last => polySpan(init)(last)
+    case Vector()     => span()
+  }
 
-  case class InlineTeX(code: String) extends Span
+  case class Heading(spans: Vector[Phrase], level: Int) extends Sentence {
+    def view: org.scalajs.dom.html.Element = level match {
+      case 1 => h1(polySpan(spans.map(_.view))).render
+      case 2 => h2(polySpan(spans.map(_.view))).render
+      case 3 => h3(polySpan(spans.map(_.view))).render
+      case 4 => h4(polySpan(spans.map(_.view))).render
+      case 5 => h5(polySpan(spans.map(_.view))).render
+      case 6 => h6(polySpan(spans.map(_.view))).render
+    }
+  }
 
-  case class DisplayTeX(code: String) extends Div
+  case class InlineTeX(code: String) extends Phrase {
+    def view: org.scalajs.dom.html.Element = {
+      val s = span().render
+      s.innerHTML = g.katex.renderToString(code).toString()
+      s
+    }
+  }
 
-  case class Paragraph(spans: List[Span]) extends Div
+  case class DisplayTeX(code: String) extends Sentence {
+    def view: org.scalajs.dom.html.Element = {
+      val s = div().render
+      s.innerHTML = g.katex.renderToString(code).toString()
+      s
+    }
+  }
+
+  case class Paragraph(spans: Vector[Phrase]) extends Sentence {
+    def view: org.scalajs.dom.html.Element = 
+        p(polySpan(spans.map(_.view))).render
+  }
 
   val eg = Paragraph(
-    List(Text("Something like"), InlineTeX("x^2 + y^2"), Text(" is a formula"))
+    Vector(
+      Text("Something like"),
+      InlineTeX("x^2 + y^2"),
+      Text(" is a formula")
+    )
   )
 
-  def inline[_: P]: P[Span] =
+  def inline[_: P]: P[Phrase] =
     P(" ".rep ~ "$" ~ (CharPred(x => x != '$').rep(1)).! ~ "$").map(s =>
       InlineTeX(s)
     )
 
-  def blankLine[_: P] : P[Unit] = P(" ".rep ~ "\n").rep(2)
+  def blankLine[_: P]: P[Unit] = P(" ".rep ~ "\n").rep(2)
 
-  def letter[_: P]: P[String] = !blankLine ~ CharPred(x => !Set('$', '_').contains(x)).! //.map(s => Text(s.toString()))
+  def letter[_: P]: P[String] =
+    !blankLine ~ CharPred(x => !Set('$', '_').contains(x)).! //.map(s => Text(s.toString()))
 
-//   def prepend(x: Span, ys: List[Span]): List[Span] = (x, ys) match {
-//     case (Text(a), Text(w) :: tail) => Text(a + w) :: tail
-//     case (a, bs)                    => a :: bs
-//   }
+  def word[_: P]: P[Phrase] = letter.rep(1).map(l => Text(l.mkString("")))
 
-  def word[_: P] : P[Span] = letter.rep(1).map(l => Text(l.mkString("")))
+  def bold[_: P]: P[Phrase] =
+    P("__" ~ letter.rep(1) ~ "__").map(l => Strong(l.mkString("")))
 
-  def strong[_: P] : P[Span] = P("__" ~letter.rep(1) ~ "__").map(l => Strong(l.mkString("")))
+  def ital[_: P]: P[Phrase] =
+    P("_" ~ letter.rep(1) ~ "_").map(l => Emph(l.mkString("")))
 
-  def emph[_: P] : P[Span] = P("_" ~letter.rep(1) ~ "_").map(l => Emph(l.mkString("")))
-
-
-  def span[_: P]: P[Span] = P(inline | emph | strong | word)
+  def phrase[_: P]: P[Phrase] = P(inline | ital | bold | word)
 
   def dispAhead[_: P] = P(&("$$"))
 
-  def displayMath[_: P]: P[Div] = P("$$" ~ (CharPred(x => x != '$').rep(1)).! ~ "$$").map{s => DisplayTeX(s)}
-
-  def spanSeq[_: P]: P[List[Span]] =
-    (End | P(" ".rep ~ "\n" ~ " ".rep ~ "\n" ~ " ".rep) | dispAhead).map { _ =>
-      List()
-    } | P(inline ~ " ".rep ~  spanSeq).map { case (x, ys) => x :: ys
-       // prepend(x, ys) 
-    } |
-      P(span ~  spanSeq).map { case (x, ys)               => x :: ys
-       // prepend(x, ys) 
+  def displayMath[_: P]: P[Sentence] =
+    P("$$" ~ (CharPred(x => x != '$').rep(1)).! ~ "$$").map { s =>
+      DisplayTeX(s)
     }
+
+  def spanSeq[_: P]: P[Vector[Phrase]] =
+    (End | P(" ".rep ~ "\n" ~ " ".rep ~ "\n" ~ " ".rep) | dispAhead).map { _ =>
+      Vector()
+    } | P(inline ~ " ".rep ~ spanSeq).map {
+      case (x, ys) => x +: ys
+      // prepend(x, ys)
+    } |
+      P(phrase ~ spanSeq).map {
+        case (x, ys) => x +: ys
+        // prepend(x, ys)
+      }
 
   def headHead[_: P]: P[Int] = P("#".rep(min = 1, max = 6).! ~ " ").map {
     _.size
   }
 
-  def para[_: P]: P[Div] = P(spanSeq).map(Paragraph(_))
+  def para[_: P]: P[Sentence] = P(spanSeq).map(Paragraph(_))
 
-  def heading[_: P]: P[Div] = P(headHead ~ spanSeq).map {
+  def heading[_: P]: P[Sentence] = P(headHead ~ spanSeq).map {
     case (l, s) => Heading(s, l)
   }
 
-  def div[_: P]: P[Div] = P(displayMath | heading | para)
+  def sentence[_: P]: P[Sentence] = P(displayMath | heading | para)
 
-  def divSeq[_: P]: P[List[Div]] =
+  def divSeq[_: P]: P[Vector[Sentence]] =
     (End).map { _ =>
-      List()
-    } | P(div ~ divSeq).map { case (x, ys) => x :: ys }
+      Vector()
+    } | P(sentence ~ divSeq).map { case (x, ys) => x +: ys }
 }
